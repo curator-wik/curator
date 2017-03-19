3ed03e7f0e31673ee33d4847ebae52706f5ff412
Mike Baynton <mike@mbaynton.com>
2017-03-18 23:37:07 -0500

    Rewrite task distribution amongst PatchCopy batch runners so it is even. Add a second PatchCopyBatchTest.

--- dist/src/Cpkg/PatchCopyBatchTask.php
+++ dist/src/Cpkg/PatchCopyBatchTask.php
@@ -27,13 +27,12 @@ class PatchCopyBatchTask extends CpkgBatchTask {
     /**
      * @var CpkgBatchTaskInstanceState $instance_state
      */
-    if ($last_processed_runnable_id === NULL) {
-      $start = $runner_rank * $instance_state->getNumRunnables();
-    } else {
+
+    list ($start, $end) = self::getStartEndRunnableId($instance_state, $runner_rank);
+    if ($last_processed_runnable_id !== NULL) {
+      // If not on the first incarnation, always pick up where last left off.
       $start = $last_processed_runnable_id + 1;
     }
-
-    $end = (($runner_rank + 1) * $instance_state->getNumRunnables()) - 1;
 
     return new PatchCopyBatchRunnableIterator(
       $this->fs_access,
@@ -42,5 +41,47 @@ class PatchCopyBatchTask extends CpkgBatchTask {
       $start,
       $end
     );
+  }
+
+  /**
+   * @param TaskInstanceStateInterface $instance_state
+   * @param int $runner_rank
+   * @return int[]
+   *   0: The id of the first runnable this runner should execute.
+   *   1: The id of the last runnable this runner should execute.
+   */
+  protected static function getStartEndRunnableId(TaskInstanceStateInterface $instance_state, $runner_rank) {
+    /**
+     * @var int $num_runnables_all_ranks
+     *   The (minimum) number of runnables run by all runners.
+     */
+    $num_runnables_all_ranks = floor($instance_state->getNumRunnables() / $instance_state->getNumRunners());
+    /**
+     * @var int $num_ranks_running_remainder
+     *   The number of ranks that need to run one extra runnable.
+     */
+    $num_ranks_running_remainder = $instance_state->getNumRunnables() % $instance_state->getNumRunners();
+
+    $first_rank_running_remainder = $instance_state->getNumRunners() - $num_ranks_running_remainder;
+    if ($runner_rank >= $first_rank_running_remainder) {
+      if ($runner_rank > $first_rank_running_remainder) {
+        $start = $runner_rank * ($num_runnables_all_ranks + 1);
+      } else {
+        $start = $runner_rank * $num_runnables_all_ranks;
+      }
+      $end = (($runner_rank + 1) * ($num_runnables_all_ranks + 1) - 1);
+    } else {
+      $start = $runner_rank * $num_runnables_all_ranks;
+      $end = (($runner_rank + 1) * $num_runnables_all_ranks) - 1;
+    }
+
+
+    if ($num_runnables_all_ranks === 0 && $runner_rank < $num_ranks_running_remainder) {
+      $start = $runner_rank;
+    }
+
+
+
+    return [$start, $end];
   }
 }
--- tests/Functional/Cpkg/PatchCopyBatchTest.php
+++ tests/Functional/Cpkg/PatchCopyBatchTest.php
@@ -7,7 +7,7 @@ namespace Curator\Tests\Functional\Cpkg;
 use Curator\Tests\Functional\WebTestCase;
 
 class PatchCopyBatchTest extends CpkgWebTestCase {
-  public function testMultiplePatchesAndCopies() {
+  public function _testMultiplePatchesAndCopies($cpkg_path, $expected_files) {
     $initial_dirs = ['subdir'];
     $initial_files = [
       'patch_target_1' => 'The quick brown fox jumps over the lazy dog\'s back.
@@ -19,7 +19,12 @@ Little boxes on the hillside.'
     ];
 
     $expected_dirs = ['subdir', 'empty_subdir'];
-    $expected_files = [
+
+    $this->_testCpkgBatchApplication($cpkg_path, $initial_dirs, $expected_dirs, $initial_files, $expected_files, 1);
+  }
+
+  public function testMultipleFilesAndCopies() {
+    $this->_testMultiplePatchesAndCopies('multiple-files-patches.zip', [
       'copied_file_1' => 'this is a new file',
       'subdir/copied_file_2' => 'this is another new file',
       'patch_target_1' => 'The quick brown fox jumps over the lazy dog\'s back.
@@ -28,8 +33,20 @@ Little boxes on the hillside.',
       'subdir/patch_target_2' => 'The quick brown fox jumps over the lazy dog\'s back.
 This is the second file that has been patched.
 Little boxes on the hillside.'
-    ];
+    ]);
+  }
 
-    $this->_testCpkgBatchApplication('multiple-files-patches.zip', $initial_dirs, $expected_dirs, $initial_files, $expected_files, 1);
+  public function testMultiplePatchesAndCopies2() {
+    $this->_testMultiplePatchesAndCopies('multiple-files-patches2.zip', [
+      'copied_file_1' => 'this is a new file',
+      'copied_file_2' => 'this is another new file',
+      'subdir/copied_file_2' => 'this is another new file',
+      'patch_target_1' => 'The quick brown fox jumps over the lazy dog\'s back.
+This is the first file that has been patched.
+Little boxes on the hillside.',
+      'subdir/patch_target_2' => 'The quick brown fox jumps over the lazy dog\'s back.
+This is the second file that has been patched.
+Little boxes on the hillside.'
+    ]);
   }
 }
